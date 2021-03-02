82579024efc78b3aad2a146b4d5f6d2a612b5bb1
Garrett Martin <me@garrettmartin.net>
2021-03-02 03:30:33 -0800

    implement fizzbuzz method to produce "fizz" on multiples of three

--- garrettmartin_hw7.py
+++ garrettmartin_hw7.py
@@ -1,2 +1,14 @@
-def fizzbuzz(x):
-    pass
+def fizzbuzz(idx=99):
+    val = idx
+    list = []
+
+    for x in range(1,101):
+        if (x % 3 == 0):
+            list.append("Fizz")
+        else:
+            list.append(str(x))
+
+    if idx != 99:
+        return(list[idx])
+    else:
+        return(print(*list))
