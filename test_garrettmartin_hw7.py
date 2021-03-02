d14ea70ee4e7ed4782ed8a427fa9a9bc3eea328d
Garrett Martin <me@garrettmartin.net>
2021-03-02 03:29:41 -0800

    correct typo in test case, define fizzbuzz, add .gitignore

--- test_garrettmartin_hw7.py
+++ test_garrettmartin_hw7.py
@@ -6,7 +6,7 @@ class TestClass(unittest.TestCase):
 # Tests for fizzbuzz method
 #-----------------------------------------------------------------------------
     def test_fizz(self):
-        self.assertEqual(fizzbuzz.fizzbuzz(2), "Fizz")
+        self.assertEqual(garrettmartin_hw7.fizzbuzz(2), "Fizz")
 
 if __name__ == "__main__":
     unittest.main()
