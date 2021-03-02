7409d7013e694acd74db2b43adaff0e79c0f19f7
Garrett Martin <me@garrettmartin.net>
2021-03-02 03:31:28 -0800

    add test_buzz() to test if multiples of 5 are replaced with "buzz"

--- test_garrettmartin_hw7.py
+++ test_garrettmartin_hw7.py
@@ -8,5 +8,8 @@ class TestClass(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(garrettmartin_hw7.fizzbuzz(2), "Fizz")
 
+    def test_buzz(self):
+        self.assertEqual(garrettmartin_hw7.fizzbuzz(4), "Buzz")
+
 if __name__ == "__main__":
     unittest.main()
